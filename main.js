de48ed16851b3d7fb8ecace99ec181041c74f6d1
ali-alim <ali.mamedov.2016@gmail.com>
2022-04-23 16:49:46 +0400

    style faqs

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 //change navbar styles on scroll
 
-window.addEventListener('scroll', () => {
-    document.querySelector('nav').classList.toggle
-    ('window-scroll', window.scrollY > 0)
-})
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scroll", window.scrollY > 0);
+});
+
+// show/hide FAQ answers
+
+const faqs = document.querySelectorAll(".faq");
+
+
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("open");
+  });
+
+});
