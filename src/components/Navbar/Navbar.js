b99849458cfaae6096611aa95d02b5aaa8282d9d
ali-alim <ali.mamedov.2016@gmail.com>
2022-04-24 14:31:34 +0400

    update index.css

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,30 @@
-import "./navbar.scss"
+import "./navbar.scss";
 
 export default function Navbar() {
   return (
     <div>
-        
+      <nav>
+        <div class="container nav__container">
+          <a href="index.html">
+            <h4>ALI M.</h4>
+          </a>
+
+          <ul class="nav__menu">
+            <li>
+              <a href="index.html">Home</a>
+            </li>
+            <li>
+              <a href="projects.html">Projects</a>
+            </li>
+            <li>
+              <a href="developers.html">Developers</a>
+            </li>
+            <li>
+              <a href="contact.html">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
