64ea09544036be935356df578a4c61e9d65e0666
ali-alim <ali.mamedov.2016@gmail.com>
2022-04-24 14:51:20 +0400

    style Categories component

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -16,54 +16,12 @@ export default function Categories() {
             <a href="./projects/web3.html">
               <article class="category">
                 <span class="category__icon">
-                  <img src="./assets//blockchain.jpg" alt="blockchain_icon" />
+                  <img src="" alt="blockchain_icon" />
                 </span>
                 <h5>WEB 3.0 Projects</h5>
                 <p>Technologies used: SOLIDITY & REACT.js</p>
               </article>
             </a>
-
-            <article class="category">
-              <span class="category__icon">
-                <img src="./assets//vanilla_js.jpg" alt="vanilla_js_icon" />
-              </span>
-              <h5>Vanilla JS</h5>
-              <p>Pure Javascript with HTML and CSS</p>
-            </article>
-
-            <a href="./projects/reactjs.html">
-              <article class="category">
-                <span class="category__icon">
-                  <img src="./assets//react.jpg" alt="react_icon" />
-                </span>
-                <h5>React JS</h5>
-                <p>One of the best Javascript Front-End Libraries</p>
-              </article>
-            </a>
-
-            <article class="category">
-              <span class="category__icon">
-                <img src="./assets//blockchain.jpg" alt="blockchain_icon" />
-              </span>
-              <h5>Blockchain</h5>
-              <p>Blockchain development is the future of web development</p>
-            </article>
-
-            <article class="category">
-              <span class="category__icon">
-                <img src="./assets//blockchain.jpg" alt="blockchain_icon" />
-              </span>
-              <h5>Blockchain</h5>
-              <p>Blockchain development is the future of web development</p>
-            </article>
-
-            <article class="category">
-              <span class="category__icon">
-                <img src="./assets//blockchain.jpg" alt="blockchain_icon" />
-              </span>
-              <h5>Blockchain</h5>
-              <p>Blockchain development is the future of web development</p>
-            </article>
           </div>
         </div>
       </section>
